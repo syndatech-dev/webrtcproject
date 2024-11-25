125c337d551da6d502115477b4a5f3bef9c71c2f
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-11-25 20:20:12 +0100

    Update webrtc_sip_controller.py

--- synda_webrtc_sip_client/controllers/webrtc_sip_controller.py
+++ synda_webrtc_sip_client/controllers/webrtc_sip_controller.py
@@ -1,2 +1,16 @@
-from odoo import http
-from odoo.http import request
+from odoo import models, fields
+
+class WebRTCSipClient(models.Model):
+    _name = 'synda.webrtc.sip.client'
+    _description = 'WebRTC SIP Client'
+
+    name = fields.Char('SIP Client Name', required=True)
+    sip_extension = fields.Char('SIP Extension', required=True)
+    sip_password = fields.Char('SIP Password', required=True)
+    sip_server = fields.Char('SIP Server', required=True)
+    user_id = fields.Many2one('res.users', string='User')
+    registration_status = fields.Selection([('registered', 'Registered'), ('unregistered', 'Unregistered')], default='unregistered')
+
+    def register_sip(self):
+         Logic to register SIP extension (via JavaScript integration with JsSIP)
+        pass
