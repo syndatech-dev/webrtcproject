6afcdc39bcee12bcd0dee8e9ad80c6e9d70bf6d7
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-04 20:38:27 +0100

    ok

--- webRTC_VERSION_1/11/webRTC/webRTC/odoo_module/models/res_users_extension.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ResUsersExtension(models.Model):
-    _inherit = 'res.users'
-
-    sip_extension = fields.Char(string='SIP Extension')
-    sip_password = fields.Char(string='SIP Password')
