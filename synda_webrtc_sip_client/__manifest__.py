d8c6dd976675b6da1eea79610a4326561553bb7e
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-05 05:55:08 +0100

    Update __manifest__.py

--- synda_webrtc_sip_client/__manifest__.py
+++ synda_webrtc_sip_client/__manifest__.py
@@ -12,7 +12,6 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            
             'synda_webrtc_sip_client/static/src/js/webrtc_client.js',
             'synda_webrtc_sip_client/static/src/css/webrtc_client.css',
         ],
