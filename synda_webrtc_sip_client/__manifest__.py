59f1b14ca858311f67a7c690e0c847be207f532d
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-08 10:06:19 +0100

    all update

--- synda_webrtc_sip_client/__manifest__.py
+++ synda_webrtc_sip_client/__manifest__.py
@@ -1,22 +1,21 @@
-
 {
     'name': 'WebRTC SIP Client',
     'version': '1.0',
     'category': 'Communication',
     'summary': 'WebRTC SIP Client for Odoo',
-    'description': 'A standalone WebRTC SIP client using JsSIP.',
+    'description': 'A WebRTC SIP client to register extensions and make calls via FreePBX.',
     'author': 'Synda Tech',
     'depends': ['base', 'web'],
     'data': [
-        'views/webrtc_sip_template.xml',
-        'views/webrtc_sip_action.xml',
-        'views/webrtc_sip_menu.xml',
+        'views/webrtc_sip_template.xml',  # Modèle HTML
+        'views/webrtc_sip_action.xml',   # Définition de l'action
+        'views/webrtc_sip_menu.xml',     # Menu principal
     ],
     'assets': {
-        'web.assets_frontend': [
-            'views/js/webrtc_client.js',
-            'views/src/js/webrtc_sip_action.js',
-            'static/src/css/webrtc_client.css',
+        'web.assets_backend': [
+            'synda_webrtc_sip_client/static/src/js/webrtc_sip_action.js',  # Action JavaScript
+            'synda_webrtc_sip_client/static/src/js/webrtc_client.js',      # Script WebRTC client
+            'synda_webrtc_sip_client/static/src/css/webrtc_client.css',    # Styles CSS
         ],
     },
     'installable': True,
