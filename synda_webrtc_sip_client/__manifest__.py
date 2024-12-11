4465b5ef679482f79fc9fde8dc47ac6466a37deb
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-11 21:32:15 +0100

    Correction de nouveau bug

--- synda_webrtc_sip_client/__manifest__.py
+++ synda_webrtc_sip_client/__manifest__.py
@@ -4,34 +4,26 @@
     'category': 'Communication',
     'summary': 'WebRTC SIP Client for Odoo',
     'description': """
-        A WebRTC SIP client integrated into Odoo to register extensions and make calls via FreePBX.
-        Features:
-        - Register SIP accounts.
-        - Make and receive calls.
-        - Integration with FreePBX systems.
+        Un module Odoo permettant l'intégration d'un client SIP basé sur WebRTC.
+        - Enregistrement des comptes SIP.
+        - Gestion des appels.
     """,
-    'author': 'Synda Tech',
+    'author': 'Foko Junior',
     'website': 'https://syndatech.com',
-    'depends': [
-        'base',  # Base Odoo module
-        'web',   # Module web pour les actions frontales
-    ],
+    'license': 'LGPL-3',
+    'depends': ['base', 'web'],
     'data': [
-        'views/webrtc_sip_template.xml',  # Template HTML pour l'interface utilisateur
-        'views/webrtc_sip_action.xml',   # Actions pour les menus ou boutons
-        'views/webrtc_sip_menu.xml',     # Menu principal
+        'views/webrtc_sip_action.xml',
+        'views/webrtc_sip_menu.xml',
+        'views/webrtc_sip_template.xml',
     ],
     'assets': {
         'web.assets_backend': [
-            'static/src/js/webrtc_sip_action.js',  # Actions JS (enregistre les actions)
-            'static/src/js/webrtc_client.js',      # Client WebRTC
-            'static/src/css/webrtc_client.css',    # Styles CSS personnalisés
-        ],
-        'web.assets_qweb': [
-            'views/webrtc_sip_template.xml',  # Inclure uniquement les templates QWeb
+            'synda_webrtc_sip_client/static/src/js/webrtc_sip_action.js',
+            'synda_webrtc_sip_client/static/src/css/webrtc_sip_client.css',
         ],
     },
     'installable': True,
     'application': True,
-    'auto_install': False,  # Important pour éviter l'installation automatique avec d'autres modules
+    'auto_install': False,
 }
