c6910ce5671d7525622d9adce3e7f2b84d29c091
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-12 17:56:15 +0100

    update

--- synda_webrtc_sip_client/__manifest__.py
+++ synda_webrtc_sip_client/__manifest__.py
@@ -1,10 +1,16 @@
 {
-    'name': 'Synda WebRTC SIP Client',
-    'version': '1.0',
-    'summary': 'Module for managing SIP clients in Odoo',
-    'category': 'Tools',
-    'author': 'Foko',
-    'website': 'http://syndatech.com',
+    'name': 'WebRTC SIP Client',
+    'version': '1.0.0',
+    'category': 'Communication',
+    'summary': 'WebRTC SIP Client for Odoo',
+    'description': """
+        Un module Odoo permettant l'intégration d'un client SIP basé sur WebRTC.
+        - Enregistrement des comptes SIP.
+        - Gestion des appels.
+    """,
+    'author': 'Foko Junior',
+    'website': 'https://syndatech.com',
+    'license': 'LGPL-3',
     'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
@@ -13,9 +19,10 @@
     ],
     'assets': {
         'web.assets_backend': [
-            '/synda_webrtc_sip_client/static/src/css/style.css',
+           '/synda_webrtc_sip_client/static/src/css/style.css',
         ],
     },
-    'application': True,
     'installable': True,
+    'application': True,
+    'auto_install': False,
 }
