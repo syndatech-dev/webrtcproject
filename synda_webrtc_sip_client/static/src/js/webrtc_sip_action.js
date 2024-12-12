c22cc31cba0f75857c437e6992126ecaa286b164
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-12 10:49:59 +0100

    r

--- synda_webrtc_sip_client/static/src/js/webrtc_sip_action.js
+++ synda_webrtc_sip_client/static/src/js/webrtc_sip_action.js
@@ -1,11 +1,13 @@
 odoo.define('synda_webrtc_sip_client.webrtc_sip_action', function (require) {
     "use strict";
 
-    const AbstractAction = require('web.AbstractAction');
-    const core = require('web.core');
+    const AbstractAction = require('web.AbstractAction'); // Module requis pour les actions
+    const core = require('web.core'); // Module pour les fonctionnalités centrales
+    const qweb = core.qweb; // QWeb pour gérer les templates si nécessaire
 
+    // Définir l'action principale
     const WebRTCSIPClient = AbstractAction.extend({
-        template: 'webrtc_sip_client',
+        template: 'webrtc_sip_client', // Nom du template défini dans le fichier XML
 
         start: function () {
             console.log("WebRTC SIP Client action loaded.");
@@ -13,6 +15,7 @@ odoo.define('synda_webrtc_sip_client.webrtc_sip_action', function (require) {
         },
     });
 
+    // Enregistrement de l'action
     core.action_registry.add('webrtc.sip.client', WebRTCSIPClient);
 
     return WebRTCSIPClient;
