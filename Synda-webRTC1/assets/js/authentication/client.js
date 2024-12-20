125a8af793c263378c1e4679ae802addf618983a
FOKO TADJUIGE B. JUNIOR <122606323+FokoJunior@users.noreply.github.com>
2024-12-20 18:32:39 +0100

    Add files via upload

---
+++ Synda-webRTC1/assets/js/authentication/client.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+        alert(result.message);
+        window.location.href = '/client/accueil.html'; // Redirection vers accueil ou dashboard
+    } else {
+        alert(result.message);
+    }
+});
